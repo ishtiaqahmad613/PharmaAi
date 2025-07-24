ec002f4823c42fcd4642b5f2d10f38b3389db648
Ishtiaq Ahmad <ishtiaqah6131919@gmail.com>
2025-07-24 14:44:58 +0500

    updated pharmaai.py with latest chatbot logic

--- pharmaai.py
+++ pharmaai.py
@@ -3,13 +3,21 @@ import streamlit as st
 def main():
     st.title("ask chatbot!")
 
+    if 'messages' not in st.session_state:
+        st.session_state.messages = []
+
+    for message in st.session_state.messages:
+        st.chat_message(message['role']).markdown(message['content'])
+
     prompt=st.chat_input("pass your prompt here")
 
     if prompt:
         st.chat_message('user').markdown(prompt)
+        st.session_state.messages.append({'role':'user','content': prompt})
 
-        response="Hi, i am  pharmaAi!"
+        response="Hi, i am  PharmaAi!"
         st.chat_message('assistant').markdown(response)
+        st.session_state.messages.append({'role':'assistant', 'content': response})
 
 if __name__ =="__main__":
     main()
